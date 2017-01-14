3a04c6de6547a8d370c9e361a7866001c019d7e9
laser <sfera.laser@gmail.com>
2017-01-15 00:51:46 +0200

    google blocked ip list for all groups

--- Google/add_blocked_ip_addresses.py
+++ Google/add_blocked_ip_addresses.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 from googleads import adwords
-
+from get_comapings import g_c
 
 CAMPAIGN_ID = 731695173
 
+iplist = ['195.39.210.0', '195.39.210.1', '195.39.210.2', '195.39.210.3', '195.39.210.4',
+                '195.39.210.5', '195.39.210.6', '195.39.210.7', '195.39.210.8', '195.39.210.9']
 
-def main(client, campaign_id, location_feed_id=None):
+def main(client, campaign_id, ipaddr):
     # Initialize appropriate service.
     campaign_criterion_service = client.GetService(
         'CampaignCriterionService', version='v201609')
@@ -15,7 +17,7 @@ def main(client, campaign_id, location_feed_id=None):
         'campaignId': campaign_id,
         'criterion': {
             'xsi_type': 'IpBlock',
-            'ipAddress': '1.3.8.0/24'
+            'ipAddress': ipaddr
         }
     }
 
@@ -42,5 +44,8 @@ def main(client, campaign_id, location_feed_id=None):
 if __name__ == '__main__':
     # Initialize client object.
     adwords_client = adwords.AdWordsClient.LoadFromStorage("/home/mushroom/Документы/ls/ya/LS/Google/googleads.yaml")
+    campaignidlist = g_c(adwords_client)
 
-main(adwords_client, CAMPAIGN_ID)
+for camp in campaignidlist:
+    for ipaddr in iplist:
+        main(adwords_client, camp, ipaddr)
